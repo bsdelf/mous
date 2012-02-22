f3610ede05cdc6b5a2c23cac1e4dd2ed61bffab0
Yanhui Shen <shen.elf@gmail.com>
2012-02-23 00:04:28 +0800

    +Use my sqt lib.

---
+++ qt/QMous/sqt/UiHelper.hpp
@@ -0,0 +1,44 @@
+#ifndef SQT_UIHELPER_HPP
+#define SQT_UIHELPER_HPP
+
+#include <QtGui>
+#include <QtCore>
+
+namespace sqt {
+
+static void adjustAllStackPages(QStackedWidget* stack, QSizePolicy plicy)
+{
+    for (int i = 0; i < stack->count(); ++i)
+    {
+        stack->widget(i)->setSizePolicy(plicy);
+    }
+}
+
+static void switchStackPage(QStackedWidget* stack, int index)
+{
+    const int pageCount = stack->count();
+    if (pageCount != 0)
+    {
+        QSizePolicy policyMin(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        QSizePolicy policyMax(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+        // minimal previous page
+        if (stack->currentIndex() != index)
+        {
+            stack->currentWidget()->setSizePolicy(policyMin);
+        }
+        else
+        {
+            // minimal all page
+            adjustAllStackPages(stack, policyMin);
+        }
+        // show and maximal new page
+        stack->setCurrentIndex(index);
+        stack->currentWidget()->setSizePolicy(policyMax);
+        stack->adjustSize();
+    }
+}
+
+}
+
+#endif
