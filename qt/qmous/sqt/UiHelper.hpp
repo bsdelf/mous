9b859e35adb81dc2d5733f451cec6220809786b3
Yanhui Shen <shen.elf@gmail.com>
2012-03-05 02:09:19 +0800

    +setActionSeparator()

--- qt/qmous/sqt/UiHelper.hpp
+++ qt/qmous/sqt/UiHelper.hpp
@@ -6,7 +6,16 @@
 
 namespace sqt {
 
-static void adjustAllStackPages(QStackedWidget* stack, QSizePolicy plicy)
+static inline void  setActionSeparator(QList<QAction*> list)
+{
+    for (int i = 0; i < list.size(); ++i) {
+        QAction* action = list[i];
+        if (action->text().isEmpty())
+            action->setSeparator(true);
+    }
+}
+
+static inline void adjustAllStackPages(QStackedWidget* stack, QSizePolicy plicy)
 {
     for (int i = 0; i < stack->count(); ++i)
     {
@@ -14,7 +23,7 @@ static void adjustAllStackPages(QStackedWidget* stack, QSizePolicy plicy)
     }
 }
 
-static void switchStackPage(QStackedWidget* stack, int index)
+static inline void switchStackPage(QStackedWidget* stack, int index)
 {
     const int pageCount = stack->count();
     if (pageCount != 0)
