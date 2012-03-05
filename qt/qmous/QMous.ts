c186dba31da61dd7adb2cf9e1d006c674f3061e6
Yanhui Shen <shen.elf@gmail.com>
2012-03-05 18:59:27 +0800

    +translation

---
+++ qt/qmous/QMous.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="MainWindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="101"/>
+        <source>NOR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="347"/>
+        <source>Tab 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.ui" line="373"/>
+        <source>Tab 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="MainWindow.cpp" line="104"/>
+        <source>Preference</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimplePlayListView</name>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="19"/>
+        <source>Append</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="24"/>
+        <source>Remove</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="29"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="34"/>
+        <source>Cut</source>
+        <translation>剪切</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="39"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="46"/>
+        <source>Tagging</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="51"/>
+        <source>Convert</source>
+        <translation>格式转换</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="56"/>
+        <source>Properties</source>
+        <translation>文件属性</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="63"/>
+        <source>Playlist</source>
+        <translation>播放列表</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="68"/>
+        <source>Load</source>
+        <translation>载入</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="72"/>
+        <source>Rename</source>
+        <translation>重命名</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="76"/>
+        <source>Save As</source>
+        <translation>另存为</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="119"/>
+        <source>Open Media</source>
+        <translation>打开媒体文件</translation>
+    </message>
+    <message>
+        <location filename="SimplePlayListView.cpp" line="119"/>
+        <source>*</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
