7d79430a710929b9f6ed508ff1ee308a47616bb6
Yanhui Shen <shen.elf@gmail.com>
2012-03-20 14:41:28 +0800

    +pass by ref, so the specified val can be checked

--- sdk/plugin/IRenderer.h
+++ sdk/plugin/IRenderer.h
@@ -15,7 +15,7 @@ public:
     virtual EmErrorCode OpenDevice(const std::string& path) = 0;
     virtual void CloseDevice() = 0;
 
-    virtual EmErrorCode SetupDevice(int32_t channels, int32_t sampleRate, int32_t bitsPerSample) = 0;
+    virtual EmErrorCode SetupDevice(int32_t& channels, int32_t& sampleRate, int32_t& bitsPerSample) = 0;
     virtual EmErrorCode WriteDevice(const char* buf, uint32_t len) = 0;
 
     // 0(muted) to 100(max)
