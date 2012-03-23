3981aa83c4ca0b515f05e4f3f2679e1d2972eee8
Yanhui Shen <shen.elf@gmail.com>
2012-03-23 19:10:46 +0800

    +IconvHelper

---
+++ sdk/scx/IconvHelper.hpp
@@ -0,0 +1,79 @@
+#ifndef SCX_ICONV_HELPER_HPP
+#define SCX_ICONV_HELPER_HPP
+
+#include <iconv.h>
+#include <string>
+
+namespace scx {
+
+namespace IconvHelper {
+
+bool ConvFromTo(const std::string& from, const std::string& wanted, 
+        const char* originalBuf, size_t originalLen, std::string& convertedContent,
+        char* workBuf = NULL, size_t workLen = 0)
+{
+    if (from.empty() || wanted.empty())
+        return false;
+    if (from == wanted)
+        return false;
+
+    bool ok = true;
+    const char* inbuf = originalBuf;
+    size_t inleft = originalLen;
+
+    char* outstart;
+    size_t outlen;
+    char* outbuf;
+    size_t outleft;
+    if (originalLen <= workLen) {
+        outstart = workBuf;
+        outlen = workLen;
+    } else {
+        outstart = new char[originalLen+4];
+        outlen = originalLen+4;
+    }
+    outbuf = outstart;
+    outleft = outlen;
+
+    size_t converted = 0;
+    do {
+        iconv_t cd = iconv_open(wanted.c_str(), from.c_str());
+        if (cd == (iconv_t)-1) {
+            ok = false;
+            break;
+        }
+
+        errno = 0;
+        converted = iconv(cd, &inbuf, &inleft, &outbuf, &outleft);
+        if (converted != (size_t)-1) {
+            break;
+        } else if (errno != E2BIG) {
+            ok = false;
+            break;
+        }
+        inbuf = originalBuf;
+        inleft = originalLen;
+        if (outstart != workBuf) {
+            delete[] outstart;
+        }
+        outlen = (outlen << 1);
+        outstart = new char[outlen];
+        outbuf = outstart;
+        outleft = outlen;
+
+        iconv_close(cd);
+    } while(true);
+
+    if (ok) {
+        convertedContent.assign(outstart, outlen-outleft);
+    }
+    if (outstart != workBuf) {
+        delete[] outstart;
+    }
+    return ok;
+}
+
+}
+}
+
+#endif
