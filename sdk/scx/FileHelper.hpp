719b8f2b8abadd4fb3b62c66628a7c991d4f2a5c
Yanhui Shen <shen.elf@gmail.com>
2012-03-23 19:29:25 +0800

    ~sync with scx

--- sdk/scx/FileHelper.hpp
+++ sdk/scx/FileHelper.hpp
@@ -1,5 +1,5 @@
-#ifndef SCX_FILEHELP_HPP
-#define SCX_FILEHELP_HPP
+#ifndef SCX_FILE_HELPER_HPP
+#define SCX_FILE_HELPEr_HPP
 
 #include <inttypes.h>
 #include <sys/types.h>
@@ -9,14 +9,16 @@
 
 namespace scx {
 
-static inline size_t FileSize(const char* path)
+namespace FileHelper {
+
+inline size_t FileSize(const char* path)
 {
     struct stat statbuf;
     stat(path, &statbuf);
     return statbuf.st_size;
 }
 
-static inline bool CreateFile(const char* path, uint64_t size)
+inline bool CreateFile(const char* path, uint64_t size)
 {
     using namespace std;
     ofstream stream(path, ios::out);
@@ -26,13 +28,13 @@ static inline bool CreateFile(const char* path, uint64_t size)
     return (FileSize(path) == size) ? true : false;
 }
 
-static inline std::string FileSuffix(const std::string& name, char ch = '.')
+inline std::string FileSuffix(const std::string& name, char ch = '.')
 {
     size_t pos = name.find_last_of(ch);
     return name.substr(pos+1, name.size());
 }
 
-static inline std::string FileDir(const std::string& path)
+inline std::string FileDir(const std::string& path)
 {
     using namespace std;
     size_t pos = path.find_last_of('/');
@@ -45,7 +47,7 @@ static inline std::string FileDir(const std::string& path)
     }
 }
 
-static inline std::string ReadAll(const char* filename, bool asBin = false)
+inline std::string ReadAll(const char* filename, bool asBin = false)
 {
     using namespace std;
 
@@ -64,6 +66,8 @@ static inline std::string ReadAll(const char* filename, bool asBin = false)
     instream.close();
 
     return stream.str();
+}
+
 }
 }
 
