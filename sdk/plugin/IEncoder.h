5175d2f0db4353eed439c7a2600c3fdb26630baf
Yanhui Shen <shen.elf@gmail.com>
2012-03-24 16:53:23 +0800

    +IEncoder interfaces

---
+++ sdk/plugin/IEncoder.h
@@ -0,0 +1,26 @@
+#ifndef MOUS_IENCODER_H
+#define MOUS_IENCODER_H
+
+#include <inttypes.h>
+#include <common/AudioMode.h>
+#include <common/ErrorCode.h>
+
+namespace mous {
+
+class IEncoder
+{
+public:
+    virtual ~IEncoder() { }
+
+    virtual EmErrorCode EncodeUnit(char* data, uint32_t& used, uint32_t& unitCount) = 0;
+    virtual EmErrorCode FlushRest() = 0;
+
+    virtual void SetAudioMode(EmAudioMode mode) const = 0;
+    virtual void SetChannels(int32_t channels) const = 0;
+    virtual void SetSampleRate(int32_t sampleRate) const = 0;
+    virtual void SetBitRate(int32_t bitRate) const = 0;
+};
+
+}
+
+#endif
