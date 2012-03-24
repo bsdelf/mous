822c2d67f61de95e1ec50dda835e4ce9789c4428
Yanhui Shen <shen.elf@gmail.com>
2012-03-24 22:13:07 +0800

    -ArgvBase

--- sdk/common/ArgvDef.h
+++ sdk/common/ArgvDef.h
@@ -25,68 +25,69 @@ enum e {
 }
 typedef ArgvType::e EmArgvType;
 
-struct ArgvBase
+struct ArgvInt
 {
-    EmArgvType type;
     std::string desc;
-};
-
-struct ArgvInt: public ArgvBase
-{
     int32_t defaultVal;
     mutable int32_t userVal; 
 };
 
-struct ArgvFloat: public ArgvBase
+struct ArgvFloat
 {
+    std::string desc;
     double defaultVal;
     mutable double userVal; 
 };
 
-struct ArgvString: public ArgvBase
+struct ArgvString
 {
+    std::string desc;
     std::string defaultVal;
     mutable std::string userVal; 
 };
 
-struct ArgvEnumedInt: public ArgvBase
+struct ArgvEnumedInt
 {
+    std::string desc;
     std::vector<int32_t> enumedVal;
     size_t defaultChoice;
     mutable size_t userChoice; 
 };
 
-struct ArgvEnumedFloat: public ArgvBase
+struct ArgvEnumedFloat
 {
+    std::string desc;
     std::vector<double> enumedtVal;
     size_t defaultChoice;
     mutable size_t userChoice; 
 };
 
-struct ArgvEnumedString: public ArgvBase
+struct ArgvEnumedString
 {
+    std::string desc;
     std::vector<std::string> enumedVal;
     size_t defaultChoice;
     mutable size_t userChoice; 
 };
 
-struct ArgvRangedInt: public ArgvBase
+struct ArgvRangedInt
 {
+    std::string desc;
     int32_t min;
     int32_t max;
     int32_t defaultVal;
     mutable int32_t userVal; 
 };
 
-struct ArgvRangedFloat: public ArgvBase
+struct ArgvRangedFloat
 {
+    std::string desc;
     double min;
     double max;
     double defaultVal;
     mutable double userVal; 
 };
 
-#undef MOUS_COMMON_ARGVS
 }
 
 #endif
