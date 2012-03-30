3e5b283446e4d56bfd51bcbb2b43674acc82eb67
Yanhui Shen <shen.elf@gmail.com>
2012-03-30 20:57:45 +0800

    +WavEncoder

---
+++ plugins/WavEncoder/WavEncoder.cpp
@@ -0,0 +1,68 @@
+#include "WavEncoder.h"
+#include <cstring>
+
+WavEncoder::WavEncoder()
+{
+}
+
+WavEncoder::~WavEncoder()
+{
+    CloseOutput();
+}
+
+EmErrorCode WavEncoder::OpenOutput(const std::string& path)
+{
+    m_OutputFile.open(path.c_str(), ios::binary | ios::in | ios::out);
+    if (!m_OutputFile.is_open())
+        return ErrorCode::EncoderFailedToOpen;
+
+    InitWavHeader(&m_WavHeader);
+    return ErrorCode::Ok;
+}
+
+void WavEncoder::CloseOutput()
+{
+    if (m_OutputFile.is_open())
+        m_OutputFile.close();
+}
+
+EmErrorCode WavEncoder::Encode(const char* buf, uint32_t len)
+{
+    m_OutputFile.write(buf, len);
+    return ErrorCode::Ok;
+}
+
+EmErrorCode WavEncoder::FlushRest()
+{
+    m_WavHeader.dataChunkLen = (uint32_t)m_OutputFile.tellg() - (uint32_t)sizeof(WavHeader);
+    m_WavHeader.lenAfterRiff = m_WavHeader.dataChunkLen + 36;
+    m_OutputFile.seekg(0, ios::beg);
+    m_OutputFile.write((char*)&m_WavHeader, sizeof(WavHeader));
+    return ErrorCode::Ok;
+}
+
+void WavEncoder::SetChannels(int32_t channels)
+{
+    m_WavHeader.channels = channels;
+}
+
+void WavEncoder::SetSampleRate(int32_t sampleRate)
+{
+    m_WavHeader.sampleRate = sampleRate;
+}
+
+void WavEncoder::SetBitsPerSample(int32_t bitsPerSample)
+{
+    m_WavHeader.bitsPerSample = bitsPerSample;
+}
+
+void WavEncoder::InitWavHeader(WavHeader* header)
+{
+    memset(header, 0, sizeof(WavHeader));
+
+    memcpy(header->riffId, "RIFF", 4);
+    memcpy(header->riffType, "WAVE", 4);
+
+    memcpy(header->formatId, "fmt ", 4);
+    memcpy(header->dataId, "data", 4);
+}
---
+++ plugins/WavEncoder/WavEncoder.h
@@ -0,0 +1,63 @@
+#ifndef WAVENCODER_H
+#define WAVENCODER_H
+
+#include <plugin/IEncoder.h>
+#include <fstream>
+using namespace std;
+using namespace mous;
+
+#pragma pack(push, 1)
+
+struct WavHeader
+{
+    // RIFF chunk
+    char riffId[4];
+    uint32_t lenAfterRiff;
+    char riffType[4];
+
+    // format chunk
+    char formatId[4];
+    uint32_t formatChunkLen;
+    uint16_t formatTag;
+    uint16_t channels;
+    uint32_t sampleRate;
+    uint32_t avgBytesPerSec;
+    uint16_t blockAlign;
+    uint16_t bitsPerSample;
+
+    // data chunk
+    char dataId[4];
+    uint32_t dataChunkLen;
+};
+
+#pragma pack(pop)
+
+class WavEncoder
+{
+public:
+    WavEncoder();
+    virtual ~WavEncoder();
+
+    virtual EmErrorCode OpenOutput(const std::string& path);
+    virtual void CloseOutput();
+
+    virtual EmErrorCode Encode(const char* buf, uint32_t len);
+    virtual EmErrorCode FlushRest();
+
+    virtual void SetChannels(int32_t channels);
+    virtual void SetSampleRate(int32_t sampleRate);
+    virtual void SetBitsPerSample(int32_t bitsPerSample);
+
+    // reimplement this to provide options
+    virtual bool GetOptions(std::vector<const BaseOption*>& list) const { return false; };
+
+private:
+    static void InitWavHeader(WavHeader* header);
+
+private:
+    fstream m_OutputFile;
+    WavHeader m_WavHeader;
+};
+
+#endif
+
