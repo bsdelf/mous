c3293d162394ac5e257e7b3f5356c7a49d028311
Yanhui Shen <shen.elf@gmail.com>
2012-04-06 13:09:14 +0800

    +WriteTag()

--- sdk/plugin/IEncoder.h
+++ sdk/plugin/IEncoder.h
@@ -6,6 +6,7 @@
 #include <inttypes.h>
 #include <util/AudioMode.h>
 #include <util/ErrorCode.h>
+#include <util/MediaTag.h>
 #include <util/Option.h>
 
 namespace mous {
@@ -26,6 +27,11 @@ public:
     virtual void SetChannels(int32_t channels) = 0;
     virtual void SetSampleRate(int32_t sampleRate) = 0;
     virtual void SetBitsPerSample(int32_t bitsPerSample) = 0;
+
+    // reimplement this to support tagging
+    virtual void WriteTag(const MediaTag& tag) const
+    {
+    }
 
     // reimplement this to provide options
     virtual bool GetOptions(std::vector<const BaseOption*>& list) const 
