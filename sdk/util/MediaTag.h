daf8900fb7aaaac5fbbce7887abf197cfaffaf14
Yanhui Shen <shen.elf@gmail.com>
2012-04-06 12:40:11 +0800

    +MediaTag

---
+++ sdk/util/MediaTag.h
@@ -0,0 +1,28 @@
+#ifndef MEDIATAG_H
+#define MEDIATAG_H
+
+#include <string>
+#include <inttypes.h>
+
+namespace mous {
+
+struct MediaTag
+{
+    std::string title;
+    std::string artist;
+    std::string album;
+    std::string comment;
+    std::string genre;
+    int32_t year;
+    int32_t track;
+
+    MediaTag():
+        year(-1),
+        track(-1)
+    {
+    }
+};
+
+}
+
+#endif
