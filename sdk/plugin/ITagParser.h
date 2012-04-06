aef50e0011a3d435e427d921f3b42d78426ad5b0
Yanhui Shen <shen.elf@gmail.com>
2012-04-06 23:55:00 +0800

    !modify/add APIs

--- sdk/plugin/ITagParser.h
+++ sdk/plugin/ITagParser.h
@@ -19,30 +19,42 @@ public:
     virtual EmErrorCode Open(const std::string& path) = 0;
     virtual void Close() = 0;
 
-    virtual std::string GetTitle() const = 0;
-    virtual std::string GetArtist() const = 0;
-    virtual std::string GetAlbum() const = 0;
-    virtual std::string GetComment() const = 0;
-    virtual std::string GetGenre() const = 0;
-    virtual int32_t GetYear() const = 0;
-    virtual int32_t GetTrack() const = 0;
+    // tag related calls
+    virtual bool HasTag() const { return false; };
+    virtual std::string GetTitle() const { return ""; } 
+    virtual std::string GetArtist() const { return ""; }
+    virtual std::string GetAlbum() const { return ""; }
+    virtual std::string GetComment() const { return ""; }
+    virtual std::string GetGenre() const { return ""; }
+    virtual int32_t GetYear() const { return 0; }
+    virtual int32_t GetTrack() const { return 0; };
 
+    // tag edit calls
+    virtual bool CanEditTag() const { return false; }
+    virtual bool SaveTag() const { return false; }
+    virtual void SetTitle(const std::string& title) { };
+    virtual void SetArtist(const std::string& artist) { }; 
+    virtual void SetAlbum(const std::string& album) { }; 
+    virtual void SetComment(const std::string& comment) { }; 
+    virtual void SetGenre(const std::string& genre) { }; 
+    virtual void SetYear(int32_t year) { };
+    virtual void SetTrack(int32_t track) { };
+
+    // cover art calls
+    virtual bool DumpCoverArt(char*& buf, size_t& len) { }
+    virtual bool StoreCoverArt(const char* buf, size_t len) { }
+
+    // property related calls
+    virtual bool HasProperties() const = 0;
     virtual int32_t GetDuration() const = 0;
     virtual int32_t GetBitRate() const = 0;
 
-    virtual void SetTitle(const std::string& title) = 0;
-    virtual void SetArtist(const std::string& artist) = 0; 
-    virtual void SetAlbum(const std::string& album) = 0; 
-    virtual void SetComment(const std::string& comment) = 0; 
-    virtual void SetGenre(const std::string& genre) = 0; 
-    virtual void SetYear(int32_t year) = 0;
-    virtual void SetTrack(int32_t track) = 0;
-
-    virtual bool HasTag() const = 0;
-    virtual bool HasProperties() const = 0;
-
     // reimplement this to provide options
-    virtual bool GetOptions(std::vector<const BaseOption*>& list) const { return false; };
+    virtual bool GetOptions(std::vector<const BaseOption*>& list) const
+    {
+        list.clear();
+        return false; 
+    };
 };
 
 }
