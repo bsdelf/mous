373ff3299e15a006c7b6f6c49d7d83534eaef1fd
Yanhui Shen <shen.elf@gmail.com>
2012-04-07 02:18:52 +0800

    ~remove useless ';'

--- sdk/plugin/ITagParser.h
+++ sdk/plugin/ITagParser.h
@@ -20,23 +20,23 @@ public:
     virtual void Close() = 0;
 
     // tag related calls
-    virtual bool HasTag() const { return false; };
+    virtual bool HasTag() const { return false; }
     virtual std::string GetTitle() const { return ""; } 
     virtual std::string GetArtist() const { return ""; }
     virtual std::string GetAlbum() const { return ""; }
     virtual std::string GetComment() const { return ""; }
     virtual std::string GetGenre() const { return ""; }
     virtual int32_t GetYear() const { return 0; }
-    virtual int32_t GetTrack() const { return 0; };
+    virtual int32_t GetTrack() const { return 0; }
 
     // tag edit calls
     virtual bool CanEditTag() const { return false; }
-    virtual bool SaveTag() const { return false; }
-    virtual void SetTitle(const std::string& title) { };
-    virtual void SetArtist(const std::string& artist) { }; 
-    virtual void SetAlbum(const std::string& album) { }; 
-    virtual void SetComment(const std::string& comment) { }; 
-    virtual void SetGenre(const std::string& genre) { }; 
+    virtual bool SaveTag() { return false; }
+    virtual void SetTitle(const std::string& title) { }
+    virtual void SetArtist(const std::string& artist) { }
+    virtual void SetAlbum(const std::string& album) { }
+    virtual void SetComment(const std::string& comment) { }
+    virtual void SetGenre(const std::string& genre) { }
     virtual void SetYear(int32_t year) { };
     virtual void SetTrack(int32_t track) { };
 
