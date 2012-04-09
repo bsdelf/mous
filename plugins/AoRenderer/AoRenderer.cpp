4b8d2cffb0317cc8c266fa334acac034df8a7e25
Yanhui Shen <shen.elf@gmail.com>
2012-04-09 22:04:14 +0800

    ~enhanced

--- plugins/AoRenderer/AoRenderer.cpp
+++ plugins/AoRenderer/AoRenderer.cpp
@@ -1,41 +1,49 @@
 #include "AoRenderer.h"
-#include <stdlib.h>
+#include <string.h>
 
 AoRenderer::AoRenderer():
     m_Driver(-1),
     m_Device(NULL)
 {
     ao_initialize();
+
     m_Driver = ao_default_driver_id();
+
+    int channels = 2;
+    int rate = 44100;
+    int bits = 16;
+    Setup(channels, rate, bits);
 }
 
 AoRenderer::~AoRenderer()
 {
+    Close();
+
+    ao_shutdown();
 }
 
 EmErrorCode AoRenderer::Open()
 {
-    return ErrorCode::Ok;
+    m_Device = ao_open_live(m_Driver, &m_Format, NULL);
+    return m_Device != NULL ? ErrorCode::Ok : ErrorCode::RendererFailedToOpen;
 }
 
 void AoRenderer::Close()
 {
+    if (m_Device != NULL) {
+        ao_close(m_Device);
+        m_Device = NULL;
+    }
 }
 
 EmErrorCode AoRenderer::Setup(int32_t& channels, int32_t& sampleRate, int32_t& bitsPerSample)
 {
-    ao_sample_format format;
-    memset(&format, 0, sizeof(format));
-    format.channels = channels;
-    format.bits = bitsPerSample;
-    format.rate = sampleRate;
-    format.byte_format = AO_FMT_LITTLE;
-
-    m_Device = ao_open_live(m_Driver, &format, NULL);
-    if (m_Device == NULL)
-        return ErrorCode::RendererFailedToSetup;
-
-    return ErrorCode::Ok;
+    memset(&m_Format, 0, sizeof(m_Format));
+    m_Format.channels = channels;
+    m_Format.bits = bitsPerSample;
+    m_Format.rate = sampleRate;
+    m_Format.byte_format = AO_FMT_LITTLE;
+    return Open() == ErrorCode::Ok ? ErrorCode::Ok : ErrorCode::RendererFailedToSetup;
 }
 
 EmErrorCode AoRenderer::Write(const char* buf, uint32_t len)
