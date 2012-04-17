b52d1dc60aca2af92fd73773514f0ee669b2b259
Yanhui Shen <shen.elf@gmail.com>
2012-04-17 21:48:04 +0800

    !add missing head file

---
+++ mous-ncurses/Config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+namespace Config {
+
+const char* const ConfigPath = "ncurses.config";
+
+const char* const ServerIp = "server.ip";
+const char* const ServerPort = "server.port";
+
+}
+
+#endif
