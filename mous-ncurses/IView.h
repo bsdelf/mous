dd51f018ce851e086bd5977b635054cfe7d292ff
Yanhui Shen <shen.elf@gmail.com>
2012-05-04 17:19:33 +0800

    -focus*() is not forced, +ViewHelper

--- mous-ncurses/IView.h
+++ mous-ncurses/IView.h
@@ -1,6 +1,11 @@
 #ifndef IVIEW_H
 #define IVIEW_H
 
+#include <ncurses.h>
+#include <panel.h>
+
+#include <string>
+
 class IView
 {
 public:
@@ -11,11 +16,26 @@ public:
 
     virtual bool InjectKey(int key) = 0;
 
-    virtual void Show(bool shown) = 0;
+    virtual void Show(bool show) = 0;
     virtual bool IsShown() const = 0;
 
-    virtual void SetFocus(bool focused) = 0;
-    virtual bool HasFocus() const = 0;
+    virtual void SetFocus(bool focus) { }
+    virtual bool HasFocus() const { return false; }
+};
+
+namespace ViewHelper {
+
+const int LEFT_MARGIN = 4;
+
+using namespace std;
+
+static inline void PrintAtCenter(WINDOW* wnd, int y, int w, const string& str)
+{
+    // NOTE: wide character not considered presently
+    int x = (w - str.size()) / 2;
+    mvwprintw(wnd, y, x, str.c_str());
+}
+
 };
 
 #endif
